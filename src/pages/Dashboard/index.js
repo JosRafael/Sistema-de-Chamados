478e3d61e0d6ec82dff844b89747c4927031424e
JosRafael <rafaelmatiashue@gmail.com>
2024-08-02 11:37:50 -0300

    header finalizado

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -11,7 +11,7 @@ export default function Dashboard(){
     return(
         <div>
             <Header/>
-            <h1>.</h1>
+            <h1>Dashboard</h1>
             <button onClick={handleLogout}>Sair da conta</button>
         </div>
     )
