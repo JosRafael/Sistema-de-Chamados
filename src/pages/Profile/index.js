f23a32f0719c1b8cecd38a82e14144b580481174
JosRafael <rafaelmatiashue@gmail.com>
2024-08-02 16:28:50 -0300

    finalizado perfil do sistema

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,6 +6,12 @@ import { FiSettings, FiUpload } from 'react-icons/fi'
 import avatar from '../../assets/avatar.png'
 import {AuthContext} from '../../contexts/auth'
 
+import { db, storage } from '../../services/firebaseConnection'
+import { doc, updateDoc } from 'firebase/firestore'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+
+import { toast } from 'react-toastify'
+
 import './profile.css';
 
 export default function Profile(){
@@ -13,24 +19,93 @@ export default function Profile(){
   const { user, storageUser, setUser, logout } = useContext(AuthContext);
 
   const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
-  const [imageAvatar, setImageAvatar] =useState(null)
+  const [imageAvatar, setImageAvatar] = useState(null);
 
   const [nome, setNome] = useState(user && user.nome)
   const [email, setEmail] = useState(user && user.email)
 
   function handleFile(e){
     if(e.target.files[0]){
-      const image = e.target.files[0]
-      if(image.type === "image/jpeg" || image.type === 'image/png'){
+      const image = e.target.files[0];
+
+      if(image.type === 'image/jpeg' || image.type === 'image/png'){
         setImageAvatar(image)
         setAvatarUrl(URL.createObjectURL(image))
       }else{
-        alert('envie uma imagem do tipo PNG ou JPEG')
-        setImageAvatar(null)
-        return
+        alert("Envie uma imagem do tipo PNG ou JPEG")
+        setImageAvatar(null);
+        return;
       }
+
+
     }
   }
+
+ 
+  async function handleUpload(){
+    const currentUid = user.uid;
+
+    const uploadRef = ref(storage, `images/${currentUid}/${imageAvatar.name}`)
+
+    const uploadTask = uploadBytes(uploadRef, imageAvatar)
+    .then((snapshot) =>{
+      
+      getDownloadURL(snapshot.ref).then( async (downloadURL) => {
+        let urlFoto = downloadURL;
+
+        const docRef = doc(db, "users", user.uid)
+        await updateDoc(docRef, {
+          avatarUrl: urlFoto,
+          nome: nome,
+        })
+        .then(() => {
+          let data = {
+            ...user,
+            nome: nome,
+            avatarUrl: urlFoto,
+          }
+   
+          setUser(data);
+          storageUser(data);
+          toast.success("Atualizado com sucesso!")
+          
+        })
+
+      })
+
+    })
+
+  }
+ 
+  async function handleSubmit(e){
+    e.preventDefault();
+
+   if(imageAvatar === null && nome !== ''){
+     // Atualizar apenas o nome do user
+     const docRef = doc(db, "users", user.uid) 
+     await updateDoc(docRef, {
+       nome: nome,
+     })
+     .then(() => {
+       let data = {
+         ...user,
+         nome: nome,
+       }
+
+       setUser(data);
+       storageUser(data);
+       toast.success("Atualizado com sucesso!")
+
+     })
+
+   }else if(nome !== '' && imageAvatar !== null){
+     // Atualizar tanto nome quanto a foto
+     handleUpload()
+   }
+
+  }
+
+
   return(
     <div>
       <Header/>
@@ -42,13 +117,13 @@ export default function Profile(){
 
        <div className="container">
 
-        <form className="form-profile">
+        <form className="form-profile" onSubmit={handleSubmit}>
           <label className="label-avatar">
             <span>
               <FiUpload color="#FFF" size={25} />
             </span>
 
-            <input type="file" accept="image/*" onChange={handleFile}/> <br/>
+            <input type="file" accept="image/*" onChange={handleFile}  /> <br/>
             {avatarUrl === null ? (
               <img src={avatar} alt="Foto de perfil" width={250} height={250} />
             ) : (
@@ -58,7 +133,7 @@ export default function Profile(){
           </label>
 
           <label>Nome</label>
-          <input type="text" value={nome} onChange={(e)=> setNome(e.target.value)}/>
+          <input type="text" value={nome} onChange={(e) =>  setNome(e.target.value)}/>
 
           <label>Email</label>
           <input type="text" value={email} disabled={true} />
@@ -69,7 +144,7 @@ export default function Profile(){
        </div>
 
        <div className="container">
-         <button onClick={()=>logout()}className="logout-btn">Sair</button>
+         <button className="logout-btn" onClick={ () => logout() }>Sair</button>
        </div>
 
       </div>
