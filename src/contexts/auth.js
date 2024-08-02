90da0b3b5a054792ce8b8ddf8c433bf22d152c65
JosRafael <rafaelmatiashue@gmail.com>
2024-08-02 10:03:08 -0300

    validando campos de cadastro

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,21 +1,53 @@
 import { useState, useEffect, createContext } from "react";
+import {auth, db} from '../services/firebaseConnection'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {doc, getDoc, setDoc}  from "firebase/firestore";
 
 export const AuthContext = createContext({})
 
 export default function AuthProvider({children}){
     const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadinAuth]=useState(false)
 
     function signIn(email, password){
         console.log(email)
         console.log(password)
         alert("Logado com sucesso!")
     }
+    async function signUp(email,password,nome){
+        setLoadinAuth(true)
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then(async (value)=>{
+            let uid= value.user.uid
+
+            await setDoc(doc(db, "users", uid), {
+                nome: nome,
+                avatarUrl: null,
+            })
+            .then(()=>{
+                let data={
+                    uid: uid,
+                    nome: nome,
+                    email: value.user.email,
+                    avatarUrl: null
+                }
+                setUser(data)
+                setLoadinAuth(false)
+            })
+            .catch((e)=>{
+                console.error(e)
+                setLoadinAuth(false)
+            })
+        })
+    }
     return(
         <AuthContext.Provider
         value={{
             signed: !!user,
             user,
-            signIn
+            signIn,
+            signUp,
+            loadingAuth
         }}
         >
             {children}
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,55 +1,75 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import Logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
 
 export default function Cadastro() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-function handleSubmit(e) {
-  e.preventDefault();
-  if (nome !== '' && email !== '' && password !== '') {
-    alert("FAZER CADASTRO");
-  } else {
-    alert("Preencha todos os campos.");
+  const { signUp, loadingAuth } = useContext(AuthContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (nome === '' || email === '' || password === '') {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("A senha deve conter no mínimo 6 dígitos");
+      return;
+    }
+
+    await signUp(email, password, nome)
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+      })
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use') {
+          alert("O email já está em uso. Por favor, use outro email.");
+        } else {
+          alert("Ocorreu um erro ao tentar criar a conta. Por favor, tente novamente.");
+        }
+      });
   }
-}
 
   return (
     <div className="container-center">
       <div className="login">
         <div className="login-area">
-          <img src={Logo} alt="Logo do sistema"/>
+          <img src={Logo} alt="Logo do sistema" />
         </div>
 
         <form onSubmit={handleSubmit}>
           <h1>Nova conta</h1>
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="Digite seu nome"
             value={nome}
             onChange={(e) => setNome(e.target.value)}
           />
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="email@email.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <input 
-            type="password" 
+          <input
+            type="password"
             placeholder="*******"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button type="submit">Acessar</button>
+          <button type="submit">
+            {loadingAuth ? 'Carregando...' : 'Cadastrar'}
+          </button>
         </form>
 
-        <Link to="/">
-            Já possui uma conta?
-        </Link>
-      </div> 
+        <Link to="/">Já possui uma conta?</Link>
+      </div>
     </div>
   );
 }
