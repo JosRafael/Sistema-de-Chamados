d282cc1d3e10d4b481b2dc64624b44a5edeca501
JosRafael <rafaelmatiashue@gmail.com>
2024-08-01 22:57:05 -0300

    tela de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
-function Cadastro() {
-    return (
-      <div className="App">
-        <h1>Página de Cadastro</h1>
-      </div>
-    );
-  }
-  
-  export default Cadastro;
-  
+export default function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={Logo} alt="Logo do sistema"/>
+        </div>
+
+        <form>
+          <h1>Nova conta</h1>
+          <input 
+            type="text" 
+            placeholder="Digite seu nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input 
+            type="text" 
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input 
+            type="password" 
+            placeholder="*******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/">
+            Já possui uma conta?
+        </Link>
+      </div> 
+    </div>
+  );
+}
